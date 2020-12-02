02021374a25234ffc30afd4e7859d3b247e0bf9d
salomohutapea <hutapeasalomo@gmail.com>
2020-12-02 22:31:00 +0700

    Change server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, cors: {
-  origin: "https://nomnomchat.azurewebsites.net/",
-  methods: ["GET", "POST"]
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "https://nomnomchat.azurewebsites.net/",
+    methods: ["GET", "POST"]
+  }
 })
 
 app.set('views', './views')
