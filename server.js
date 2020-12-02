76614c6a3a17d685825f3090f76d000f8a294455
salomohutapea <hutapeasalomo@gmail.com>
2020-12-02 22:47:03 +0700

    Fix CORS bug

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "https://nomnomchat.azurewebsites.net/",
-    methods: ["GET", "POST"]
-  }
-})
+const io = require('socket.io')(server, { origins: '*:*'})
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
